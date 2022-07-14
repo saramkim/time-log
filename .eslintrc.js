c19048bf0da73df1aedf57743b1dfa8cb04172a7
saram <yujun1018@naver.com>
2022-07-14 10:07:13 +0900

    eslint, prettier setting

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-shadow': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react/react-in-jsx-scope': 0,
+  },
+  overrides: [
+    {
+      files: ['*.tsx', '**/*.ts', '**/*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // react-dom and react
+              ['^react$', '^react-dom', '^react-router'],
+              // external libraries
+              ['^[a-z]'],
+              // path alias
+              ['^@'],
+              // Parent imports.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Side effect imports
+              ['^\\u0000'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
