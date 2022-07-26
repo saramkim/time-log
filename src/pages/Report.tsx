b8636194b966466aaa0399151b5102020bde64f5
saram <yujun1018@naver.com>
2022-07-26 23:37:45 +0900

    Add date-selector

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -6,7 +6,7 @@ import Today from 'components/Today';
 import 'css/Report.css';
 
 function Report() {
-  const date = Today();
+  const [date, setDate] = useState(Today());
   const LSItem = JSON.parse(localStorage.getItem(date) || '{}');
   const [data, setData] = useState('');
 
@@ -17,15 +17,39 @@ function Report() {
       value: LSItem[e],
     }));
     setData(data);
-  }, []);
+  }, [date]);
 
   return (
     <div className='report-wrapper'>
+      {/* <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      > */}
+      <select
+        name='date'
+        onChange={(e) => {
+          setDate(e.target.value);
+        }}
+      >
+        <DateList />
+      </select>
+      {/* <button type='submit'>선택</button> */}
+      {/* </form> */}
+
+      {/* <input type='date' /> */}
+      {/* <input type='week' /> */}
       <PieChart data={data} />
     </div>
   );
 }
 
-// }
+function DateList() {
+  const LSKeys: any = Object.keys(window.localStorage);
+
+  return LSKeys.reverse().map((e: string) => {
+    return <option>{e}</option>;
+  });
+}
 
 export default Report;
